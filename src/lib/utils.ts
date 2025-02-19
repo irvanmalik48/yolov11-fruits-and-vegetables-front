e06ccd924b1e3b79a12c9d735f5e4a378a9f038d
Irvan Malik <irvanma@gnuweeb.org>
2025-02-20 01:35:07 +0700

    feat(utils): add `useMobileOnly` hook
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function useMobileOnly() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return isMobile;
 }
